96aa33238a5b6613898e7d92a7f0eb17ac073f9f
Nolito10 <norlanreyes14rosales@gmail.com>
2023-10-25 19:09:27 -0600

    Agregar archivos de proyecto.

---
+++ Cuenta_Bancaria/Cuenta.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cuenta_Bancaria
+{
+    internal class Cuenta
+    {
+        //Campos Estaticos
+
+        public static int CantidadDeCuentas { get; set; }
+        public static double TotalDinero { get; set; }
+
+        //Propiedades
+
+        private string _NumeroCuenta;
+        private string _Titular;
+        private double _Saldo;
+        private string _Estado;
+
+        //Getters y Setters
+        public string Estado
+        {
+            get { return _Estado; }
+            set { _Estado = value; }
+        }
+
+
+        public double Saldo
+        {
+            get { return _Saldo; }
+            set { _Saldo = value; }
+        }
+
+
+        public string Titular
+        {
+            get { return _Titular; }
+            set { _Titular = value; }
+        }
+
+
+        public string NumeroCuenta
+        {
+            get { return _NumeroCuenta; }
+            set { _NumeroCuenta = value; }
+        }
+
+        //Propiedad de Navegacion
+
+        public List<Transaccion> Transacciones { get; set; }
+
+        //Constructores
+
+        public Cuenta()
+        {
+
+        }
+        public Cuenta(string NumeroCuenta, string Titular, double Saldo, string Estado)
+        {
+
+            this._NumeroCuenta = NumeroCuenta;
+            this._Titular = Titular;
+            this._Saldo = Saldo;
+            this._Estado = Estado;
+
+            CantidadDeCuentas++; //incrementa cantidad de cuenta
+            TotalDinero += Saldo; //Sumar saldo al total
+            Transacciones = new List<Transaccion>();
+
+        }
+
+        //Metodos
+
+        public void Depositar(double Monto)
+        {
+            Saldo += Monto;
+            TotalDinero += Monto;
+            Transacciones.Add(new Transaccion("Deposito", Monto));
+        }
+
+        public void Retirar(double Monto)
+        {
+            if (Saldo >= Monto)
+            {
+                Saldo -= Monto;
+                TotalDinero -= Monto;
+                Transacciones.Add(new Transaccion("Retiro", -Monto));
+            }
+            else
+            {
+                Console.WriteLine("Saldo insuficiente para retirar esa cantidad");
+            }
+        }
+
+        public void Inactivar()
+        {
+            Estado = "Inactivo";
+        }
+
+        public void MostrarInformacion()
+        {
+            Console.WriteLine($"Numero de Cuenta:{NumeroCuenta}");
+            Console.WriteLine($"Titular:{Titular}");
+            Console.WriteLine($"Saldo:{Saldo}");
+            Console.WriteLine($"Estado:{Estado}");
+
+            Console.WriteLine("Transacciones");
+            foreach (var transaccion in Transacciones)
+            {
+                Console.WriteLine(transaccion);
+            }
+        }
+               List<Cuenta> Cuentas = new List<Cuenta>();
+        public void MostrarInformacion(string numeroCuenta)
+        {
+           
+            //Buscar Cuenta
+            Cuenta cuenta = Cuentas.Find(c => c.NumeroCuenta == numeroCuenta);
+            if (cuenta != null)
+            {
+                cuenta.MostrarInformacion();
+            }
+            else
+            {
+                Console.WriteLine("No se encontró cuenta con ese número.");
+            }
+
+        }
+        public void Transferir(string numeroOrigen, string numeroDestino, double monto, string descripcion)
+        {
+            // Buscar cuentas
+            Cuenta origen = Cuentas.Find(c => c.NumeroCuenta == numeroOrigen);
+            Cuenta destino = Cuentas.Find(c => c.NumeroCuenta == numeroDestino);
+
+
+
+            if (origen != null && destino != null)
+            {
+                origen.Retirar(monto);
+                origen.Transacciones.Add(new Transaccion($"Transferencia a {destino.NumeroCuenta}", -monto));
+
+                destino.Depositar(monto);
+                destino.Transacciones.Add(new Transaccion($"Transferencia desde {origen.NumeroCuenta}", monto));
+            }
+            else
+            {
+                Console.WriteLine("No se encontró alguna de las cuentas.");
+            }
+        }
+
+        public void Transferir(string numeroOrigen, string numeroDestino, double monto)
+        {
+            Transferir(numeroOrigen, numeroDestino, monto, "Transferencia entre cuentas");
+        }
+
+        public static void MostrarInfoBanco()
+        {
+            Console.WriteLine($"Cantidad de Cuentas: {CantidadDeCuentas}");
+            Console.WriteLine($"Total Dinero: {TotalDinero}");
+        }
+    }
+
+    public class Transaccion
+    {
+        public string Codigo { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime Fecha { get; set; }
+        public double Monto { get; set; }
+
+
+        public Transaccion(string descripcion, double monto)
+        {
+            Codigo = Guid.NewGuid().ToString();
+            Descripcion = descripcion;
+            Fecha = DateTime.Now;
+            Monto = monto;
+        }
+        public override string ToString()
+        {
+            return ($"{Codigo} {Descripcion} {Fecha} {Monto}");
+        }
+
+    }
+
+
+
+}
+
---
+++ Cuenta_Bancaria/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Cuenta_Bancaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Cuenta> Cuentas = new List<Cuenta>();
+
+            while (true)
+            {
+                Console.WriteLine("Menú:");
+                Console.WriteLine("1. Crear nueva cuenta");
+                Console.WriteLine("2. Depositar dinero");
+                Console.WriteLine("3. Retirar dinero");
+                Console.WriteLine("4. Transferir dinero");
+                Console.WriteLine("5. Mostrar información de una cuenta");
+                Console.WriteLine("6. Mostrar información del banco");
+                Console.WriteLine("7. Salir");
+                Console.Write("Por favor, selecciona una opción: ");
+
+                if (int.TryParse(Console.ReadLine(), out int opcion))
+                {
+                    switch (opcion)
+                    {
+                        case 1:
+                            Console.Write("Introduce el número de cuenta: ");
+                            string numeroCuenta = Console.ReadLine();
+                            Console.Write("Introduce el nombre del titular: ");
+                            string titular = Console.ReadLine();
+                            Console.Write("Introduce el saldo inicial: ");
+                            if (double.TryParse(Console.ReadLine(), out double saldoInicial))
+                            {
+                                Cuentas.Add(new Cuenta(numeroCuenta, titular, saldoInicial, "Activa"));
+                                Console.WriteLine("Cuenta creada exitosamente.");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Entrada inválida para saldo inicial.");
+                            }
+                            break;
+                        case 2:
+                            Console.Write("Introduce el número de cuenta: ");
+                            string numeroDeposito = Console.ReadLine();
+                            Cuenta cuentaDeposito = Cuentas.Find(c => c.NumeroCuenta == numeroDeposito);
+                            if (cuentaDeposito != null)
+                            {
+                                Console.Write("Introduce la cantidad a depositar: ");
+                                if (double.TryParse(Console.ReadLine(), out double montoDeposito))
+                                {
+                                    cuentaDeposito.Depositar(montoDeposito);
+                                    Console.WriteLine($"Depósito de {montoDeposito:C} realizado con éxito en la cuenta {numeroDeposito}.");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Cantidad inválida.");
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("No se encontró la cuenta con ese número.");
+                            }
+                            break;
+                            
+                        case 3:
+                            Console.Write("Introduce el número de cuenta: ");
+                            string numeroRetiro = Console.ReadLine();
+                            Cuenta cuentaRetiro = Cuentas.Find(c => c.NumeroCuenta == numeroRetiro);
+                            if (cuentaRetiro != null)
+                            {
+                                Console.Write("Introduce la cantidad a retirar: ");
+                                if (double.TryParse(Console.ReadLine(), out double montoRetiro))
+                                {
+                                    cuentaRetiro.Retirar(montoRetiro);
+                                    Console.WriteLine($"Retiro de {montoRetiro:C} realizado con éxito en la cuenta {numeroRetiro}.");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Cantidad inválida.");
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("No se encontró la cuenta con ese número.");
+                            }
+                            break;
+                        case 4:
+                            Console.Write("Introduce el número de cuenta de origen: ");
+                            string numeroOrigen = Console.ReadLine();
+                            Console.Write("Introduce el número de cuenta de destino: ");
+                            string numeroDestino = Console.ReadLine();
+                            Cuenta cuentaOrigen = Cuentas.Find(c => c.NumeroCuenta == numeroOrigen);
+                            Cuenta cuentaDestino = Cuentas.Find(c => c.NumeroCuenta == numeroDestino);
+                            if (cuentaOrigen != null && cuentaDestino != null)
+                            {
+                                Console.Write("Introduce la cantidad a transferir: ");
+                                if (double.TryParse(Console.ReadLine(), out double montoTransferencia))
+                                {
+                                    cuentaOrigen.Transferir(numeroOrigen, numeroDestino, montoTransferencia);
+                                    Console.WriteLine($"Transferencia de {montoTransferencia:C} realizada de la cuenta {numeroOrigen} a la cuenta {numeroDestino}.");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Cantidad inválida.");
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("No se encontró alguna de las cuentas.");
+                            }
+                            break;
+                        case 5:
+                            Console.Write("Introduce el número de cuenta: ");
+                            string numeroBuscado = Console.ReadLine();
+                            Cuenta cuentaBuscada = Cuentas.Find(c => c.NumeroCuenta == numeroBuscado);
+                            if (cuentaBuscada != null)
+                            {
+                                cuentaBuscada.MostrarInformacion();
+                            }
+                            else
+                            {
+                                Console.WriteLine("No se encontró la cuenta con ese número.");
+                            }
+                            break;
+                        case 6:
+                            Cuenta.MostrarInfoBanco();
+                            break;
+                        case 7:
+                            Console.WriteLine("¡Hasta luego!");
+                            Environment.Exit(0);
+                            break;
+                        default:
+                            Console.WriteLine("Opción no válida. Por favor, selecciona una opción del menú.");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Entrada no válida. Por favor, introduce un número correspondiente a una opción del menú.");
+                }
+
+                Console.WriteLine("Presiona cualquier tecla para continuar...");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+}
